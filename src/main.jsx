c31b3282ecc0e9f182dfc1c871f213c66c8fe460
Hafiz Burhan <larsalaxsanderson@gmail.com>
2025-09-16 16:09:55 +0500

    The Almighty push

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+// Polyfill for Node.js modules in browser
+import { Buffer } from 'buffer';
+window.Buffer = Buffer;
+window.global = globalThis;
+
+// Process polyfill
+if (!window.process) {
+  window.process = { env: {} };
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,40 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis',
+    process: {
+      env: {}
+    }
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+      stream: 'stream-browserify',
+      http: 'stream-http',
+      https: 'https-browserify',
+      url: 'url',
+      zlib: 'browserify-zlib',
+    },
+  },
+  optimizeDeps: {
+    include: [
+      'buffer',
+      'stream-browserify',
+      'stream-http',
+      'https-browserify',
+      'url',
+      'browserify-zlib',
+    ],
+    exclude: ['@safe-global/protocol-kit', '@safe-global/api-kit']
+  },
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      plugins: [],
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
 });
